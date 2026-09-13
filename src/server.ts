fda7bac0071bafb3839bce0d670d3dbef463ee7d
Hugo Pereira <hugoper_cs@yahoo.com>
2026-09-13 09:49:42 -0300

    build: add Express dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log('Server is running...');
+import express, { Request, Response, json } from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ player: "beckham" });
+});
+
+app.listen(port, () => {
+  console.log(`🔥 Server running at port http://localhost:${port}`);
+});
